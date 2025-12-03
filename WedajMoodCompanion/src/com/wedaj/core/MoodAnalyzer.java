36ec02cf9b171cb1e300b664442cb27f174d66ea
enuat200 <enasatham579@gmail.com>
2025-12-03 07:43:54 +0300

    Update MoodAnalyzer.java

--- WedajMoodCompanion/src/com/wedaj/core/MoodAnalyzer.java
+++ WedajMoodCompanion/src/com/wedaj/core/MoodAnalyzer.java
@@ -1,9 +1,9 @@
 package com.wedaj.core;
 
-// responsibility: return a Mood value (implementation can be simple later)
+
 public class MoodAnalyzer {
     public Mood detectMood(String message) {
-        // placeholder: always return NEUTRAL for now
+        
         return Mood.NEUTRAL;
     }
 }
